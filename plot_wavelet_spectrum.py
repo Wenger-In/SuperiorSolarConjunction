1a69882ed492c8a9d0649d9260af6a9da6491cf7
Wenger-In <97772906+Wenger-In@users.noreply.github.com>
2023-12-15 01:17:10 +0800

    Update plot_wavelet_spectrum.py

--- plot_wavelet_spectrum.py
+++ plot_wavelet_spectrum.py
@@ -73,8 +73,9 @@ for i in range(var.shape[1]):
     # calculate CWT
     wavename = 'cmorl1.5-1.0'
     num_freq = 50
-    # wave_freq = np.logspace(-3, np.log10(1/(2*dt)), num_freq)
-    wave_freq = np.logspace(-1, 0.8*np.log10(1/(2*dt)), num_freq)
+    wave_freq = np.logspace(-3, np.log10(1/(2*dt)), num_freq)
+    if file_type == 1:
+        wave_freq = np.logspace(-1, 0.8*np.log10(1/(2*dt)), num_freq)
     scales = 1 / wave_freq
     cwtmatr, _ = pywt.cwt(signal, scales, wavename, 1.0 / dt)
 
