eed9eb8e61a119d7b8cb3c4110d5223b4b4fa706
Wenger-In <97772906+Wenger-In@users.noreply.github.com>
2023-12-14 10:49:35 +0800

    Update plot_wavelet_spectrum.py

--- plot_wavelet_spectrum.py
+++ plot_wavelet_spectrum.py
@@ -3,58 +3,79 @@ import matplotlib.pyplot as plt
 from scipy.fft import fft
 import pywt
 
-# 读取数据文件
-data = np.loadtxt('E:/Research/Data/Tianwen/m1930x_up_new/HtHtchan3_1frephase2s.dat')
+# import data
+file_type = 1
+if file_type == 0:
+    file_dir = 'E:/Research/Data/Tianwen/m1a01x_up/'
+    file_name = 'kmkmchan3frephase1s.dat'
+    var_list = ['Residual Frequency [Hz]', 'Residual Phase [rad]', 'Signal Density [dB]', 'Noise Density [dB]']
+elif file_type == 1:
+    file_dir = 'E:/Research/Data/Tianwen/m1727x/km/amp_fre_phase/TW/'
+    file_name = 'TWkmfpda.dat'
+    var_list = ['Residual Frequency [Hz]', 'Residual Phase [rad]', 'Relative Time Delay [ns]',
+                 'Residual Frequency [Hz]', 'Residual Phase [rad]', 'Relative Time Delay [ns]',
+                 'Signal-to-Noise Ratio [dB]', 'Signal Strength [dB]']
+if file_type == 2:
+    file_dir = 'E:/Research/Data/Tianwen/m1727x/km/amp_fre_phase/TW/'
+    file_name = 'TWkmfreq_1s.dat'
+    var_list = ['Residual Frequency-1 [Hz]', 'Residual Frequency-2 [Hz]']
 
-# 提取时间列和物理量列
+file_path = file_dir + file_name
+data = np.loadtxt(file_path)
 time = data[:, 0]
-physical_quantities = data[:, 1:]
+time = time - time [0]
+var = data[:, 1:]
+if file_type == 1:
+    var = var[:, 1:]
 
-# 定义傅里叶变换和小波变换函数
-def plot_fft(time, signal, title):
+def plot_all(time, signal, fft_freq, fft_values, wave_freq, cwtmatr, title):
+    fig, axes = plt.subplots(2, 2, figsize=(12, 6), gridspec_kw={'height_ratios': [3, 5], 'width_ratios': [5, 4]})
+
+    # time series
+    axes[0, 0].plot(time, signal)
+    axes[0, 0].set_xlabel('Time [s]')
+    axes[0, 0].set_xlim(time.min(), time.max())
+    axes[0, 0].tick_params(direction='in')
+    axes[0, 0].set_title('Time Series')
+
+    # wavelet transform
+    im = axes[1, 0].imshow(np.abs(cwtmatr), aspect='auto', extent=[time[0], time[-1], wave_freq[0], wave_freq[-1]], cmap='jet', interpolation='bilinear')
+    axes[1, 0].set_title('Wavelet Transform')
+    axes[1, 0].set_ylabel('Frequency [Hz]')
+    cbar = plt.colorbar(im, ax=axes[1, 0], orientation='horizontal', pad=0.1)
+    
+    # no right-upper subplot
+    axes[0, 1].axis('off')
+    
+    # fourier transform
+    axes[1, 1].plot(fft_freq, np.abs(fft_values))
+    axes[1, 1].set_xlabel('Frequency [Hz]')
+    axes[1, 1].set_ylabel('Amplitude [dB]')
+    axes[1, 1].set_xscale('log')
+    axes[1, 1].set_yscale('log')
+    axes[1, 1].tick_params(direction='in')
+    axes[1, 1].set_title('Fourier Transform')
+
+    fig.suptitle(title)
+    plt.show()
+
+for i in range(var.shape[1]):
+    signal = var[:, i]
+    var_title = var_list[i] if i < len(var_list) else f'variable {i + 1}'
+    title = file_path[24:] + ' --- ' + var_title
+
+    # calculate FTT
     n = len(time)
     dt = time[1] - time[0]
-    freq = np.fft.fftfreq(n, dt)
+    fft_freq = np.fft.fftfreq(n, dt)
     fft_values = fft(signal)
-    plt.figure(figsize=(10, 4))
-    plt.plot(freq, np.abs(fft_values))
-    plt.title(title)
-    plt.xlabel('Frequency (Hz)')
-    plt.ylabel('Amplitude')
-    plt.show()
 
-def plot_wavelet_transform(time, signal, title):
-    scales = np.arange(1, len(time) + 1)
-    coefs, freqs = pywt.cwt(signal, scales, 'morl')
-    plt.figure(figsize=(10, 4))
-    plt.imshow(np.abs(coefs), aspect='auto', extent=[time[0], time[-1], 1, len(scales)], cmap='jet', interpolation='bilinear')
-    plt.colorbar(label='Magnitude')
-    plt.title(title)
-    plt.xlabel('Time')
-    plt.ylabel('Scale')
-    plt.show()
+    # calculate CWT
+    wavename = 'cmorl1.5-1.0'
+    num_freq = 100
+    wave_freq = np.logspace(-3, np.log10(1/(2*dt)), num_freq)
+    scales = 1 / wave_freq
+    cwtmatr, _ = pywt.cwt(signal, scales, wavename, 1.0 / dt)
 
-# 绘制原始物理量的时间序列图
-for i in range(physical_quantities.shape[1]):
-    signal = physical_quantities[:, i]
-    title = f'Physical Quantity {i + 1} - Time Series'
-    
-    plt.figure(figsize=(10, 4))
-    plt.plot(time, signal)
-    plt.title(title)
-    plt.xlabel('Time')
-    plt.ylabel('Amplitude')
-    plt.show()
-
-
-# 对每一列物理量进行傅里叶变换和小波变换
-for i in range(physical_quantities.shape[1]):
-    signal = physical_quantities[:, i]
-    title_fft = f'FFT - Physical Quantity {i + 1}'
-    title_wavelet = f'Wavelet Transform - Physical Quantity {i + 1}'
-
-    # 绘制傅里叶变换谱
-    plot_fft(time, signal, title_fft)
-
-    # 绘制小波变换谱
-    # plot_wavelet_transform(time, signal, title_wavelet)
+    # plot figure
+    plot_all(time, signal, fft_freq[fft_freq > 0], fft_values[fft_freq > 0], wave_freq, cwtmatr, title)
