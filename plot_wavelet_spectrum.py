36dae6c2b691bb7afc7273195a097026de53e1b2
Wenger-In <97772906+Wenger-In@users.noreply.github.com>
2023-12-20 22:08:14 +0800

    Update plot_wavelet_spectrum.py

--- plot_wavelet_spectrum.py
+++ plot_wavelet_spectrum.py
@@ -1,9 +1,11 @@
 import numpy as np
+from numpy.polynomial import Polynomial
 import matplotlib.pyplot as plt
 import pywt
+from matplotlib.colors import LogNorm
 
 # import data
-file_type = 3
+file_type = 0
 if file_type == 0:
     file_dir = 'E:/Research/Data/Tianwen/m1a04x_renew/'
     file_name = 'BdBdchan3_1frephase2s.dat'
@@ -23,26 +25,26 @@ elif file_type == 2:
     unit_list = ['Hz', 'Hz']
 elif file_type == 3:
     file_dir = 'E:/Research/Data/Tianwen/m1a04x_PLL/'
-    file_name = 'HhHhsignum113pll_Ts.dat'
+    file_name = 'BdBdsignum113pll_Ts.dat'
     var_list = ['Residual Frequency-1', 'Residual Phase-1',
                  'Residual Frequency-2', 'Residual Phase-2']
     unit_list = ['Hz', 'Hz']
 
 file_path = file_dir + file_name
 data = np.loadtxt(file_path)
-# data = data[8000:,:]
+# data = data[8000:,:] # for HhHhsignum113pll_Ts.dat
 time = data[:, 0]
 var = data[:, 1:]
 if file_type == 2:
     time = data[:, 1]
     var = data[:, 2:]
-time = time - time [0]
+time = time - time[0]
 if file_type == 0:
     time = np.linspace(0,int(file_name[19])*(len(time)-1),len(time))
 if file_type == 1:
     var = var[:, 1:]
 
-def plot_all(time, signal, dt, n, fft_freq, fft_values, wave_freq, cwtmatr, cwt_freq, title, var_name, unit_name):
+def plot_all(time, signal, fft_freq, fft_ps, cwt_freq, cwt_ps, title, var_name, unit_name):
     fig, axes = plt.subplots(2, 2, figsize=(12, 6), gridspec_kw={'height_ratios': [3, 5], 'width_ratios': [5, 4]})
 
     # time series
@@ -54,7 +56,7 @@ def plot_all(time, signal, dt, n, fft_freq, fft_values, wave_freq, cwtmatr, cwt_
     axes[0, 0].set_title('Time Series')
 
     # wavelet transform
-    pcwt = axes[1, 0].pcolormesh(time, cwt_freq, np.abs(cwtmatr)**2/n, cmap='jet')
+    pcwt = axes[1, 0].pcolormesh(time, cwt_freq, cwt_ps, cmap='jet', norm=LogNorm())
     axes[1, 0].set_title('Wavelet Transform')
     axes[1, 0].set_ylabel('Frequency [Hz]')
     axes[1, 0].set_yscale('log')
@@ -64,7 +66,7 @@ def plot_all(time, signal, dt, n, fft_freq, fft_values, wave_freq, cwtmatr, cwt_
     axes[0, 1].axis('off')
     
     # fourier transform
-    axes[1, 1].plot(fft_freq, np.abs(fft_values)**2/n)
+    axes[1, 1].plot(fft_freq, fft_ps)
     axes[1, 1].set_xlabel('Frequency [Hz]')
     axes[1, 1].set_ylabel('Spectral Density [' + unit_name +'^2/Hz]')
     axes[1, 1].set_xscale('log')
@@ -81,6 +83,18 @@ def eliminate_weekly_ambiguity(phase_sequence):
             phase_sequence[i:] -= 2 * np.pi
     return phase_sequence
 
+def linear_detrend(phase_sequence, time, title):
+    coefficients = np.polyfit(time, phase_sequence, 1)
+    fit_sequence = np.polyval(coefficients, time)
+    detrended_sequence = phase_sequence - fit_sequence
+    plt.figure()
+    plt.plot(time,phase_sequence,label='phase')
+    plt.plot(time,fit_sequence,label='fit')
+    plt.legend()
+    plt.title(title)
+    plt.show()
+    return detrended_sequence
+
 for i in range(var.shape[1]):
     signal = var[:, i]
     var_name = var_list[i] if i < len(var_list) else f'variable {i + 1}'
@@ -90,13 +104,19 @@ for i in range(var.shape[1]):
     # eliminate ambiguity for phase sequence
     if file_type == 0 and i == 1:
         signal = eliminate_weekly_ambiguity(signal)
+    
+    # linear detrend for phase sequence
+    if file_type == 3 and (i == 0 or i == 3):
+        signal = linear_detrend(signal, time, title)
 
     # calculate FTT
     n = len(time)
     dt = time[1] - time[0]
     print(dt)
     fft_freq = np.fft.fftfreq(n, dt)
-    fft_values = np.fft.fft(signal)
+    fft_amp = np.fft.fft(signal)
+    fft_amp = fft_amp[fft_freq > 0]
+    fft_ps = np.abs(fft_amp)**2 / n
 
     # calculate CWT
     wavename = 'cmorl1.5-1.0'
@@ -106,7 +126,7 @@ for i in range(var.shape[1]):
         wave_freq = np.logspace(-1, 0.8*np.log10(1/(2*dt)), num_freq)
     scales = 1 / wave_freq
     cwtmatr, cwt_freq = pywt.cwt(signal, scales, wavename, 1.0 / dt)
-    # print(cwt_freq)
+    cwt_ps = np.abs(cwtmatr)**2 / n
 
     # plot figure
-    plot_all(time, signal, dt, n, fft_freq[fft_freq > 0], fft_values[fft_freq > 0], wave_freq, cwtmatr, cwt_freq, title, var_name, unit_name)
+    plot_all(time, signal, fft_freq[fft_freq > 0], fft_ps, cwt_freq, cwt_ps, title, var_name, unit_name)
