2064ded656b382cbe14208937538cb3b32620fd0
Wenger-In <97772906+Wenger-In@users.noreply.github.com>
2024-03-21 21:28:15 +0800

    coherence

--- coherence_wavelet_reconstruct.py
+++ coherence_wavelet_reconstruct.py
@@ -6,6 +6,12 @@ from matplotlib.colors import SymLogNorm
 from scipy.stats import zscore
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+from scipy.interpolate import interp1d, CubicSpline
+
+import sys
+sys.path.append(r'E:/Research/Program/else')
+import python_utils_JSHEPT
+from python_utils_JSHEPT import get_plot_WaveletAnalysis_of_var_vect, wavelet_reconstruction
 
 ## function: convert the time data to 'seconds of day'
 def convert_to_second_of_day(time_array):
@@ -27,6 +33,8 @@ def convert_to_second_of_day(time_array):
 def eliminate_outliers(freq, t, threshold):
     freq_zscore = zscore(freq)
     outliers = np.abs(freq_zscore) > threshold
+    outliers[0] = False
+    outliers[-1] = False
     freq = freq[~outliers]
     t = t[~outliers]
     return freq, t
@@ -62,7 +70,7 @@ def detrend(freq, t, n, title):
 
 ## import data
 i_case = 4
-save_or_not = 1
+save_or_not = 0
 if i_case == 1: # 2021/09/30(273), 12:00-13:00, Ht-Sv, Ht-Wz, Sv-Wz, (latitudinal fluctuaion)
     file_dir = 'E:/Research/Data/Tianwen/m1930x_up_new/'
     save_dir = 'E:/Research/Work/tianwen_IPS/m1930x_up_new/'
@@ -82,8 +90,8 @@ elif i_case == 2: # 2021/10/04(277), 05:40-08:20, Js-Bd, Bd-Yg, Yg-Hh, (inward p
     file_Hh = 'HhHhchan3_1frephase1s.dat'
     file1_name = file_Yg
     file2_name = file_Hh
-    time_beg = 2021277080000
-    time_end = 2021277083000
+    time_beg = 2021277053000
+    time_end = 2021277060000
 elif i_case == 3: # 2021/10/07(280), 03:30-04:00, sh-km, (polar region fluctuation)
     file_dir = 'E:/Research/Data/Tianwen/m1a07x_up/'
     save_dir = 'E:/Research/Work/tianwen_IPS/m1a07x_up/'
@@ -100,10 +108,10 @@ elif i_case == 4: # 2021/10/15(288), 01:00-04:00, Bd-Hh, Bd-Ys, Hh-Ys, (fine str
     file_Bd = 'BdBdchan3_1frephase1s.dat'
     file_Hh = 'HhHhchan3_1frephase1s.dat'
     file_Ys = 'YsYschan3_1frephase1s.dat'
-    file1_name = file_Hh
-    file2_name = file_Ys
-    time_beg = 2021288123000
-    time_end = 2021288130000
+    file1_name = file_Bd
+    file2_name = file_Hh
+    time_beg = 2021288073000
+    time_end = 2021288080000
 # convert subplot time interval
 str_beg = str(time_beg)[-6:-2]
 str_end = str(time_end)[-6:-2]
@@ -150,79 +158,53 @@ freq2_out = detrend(freq2_out, sod2_out, 3, file2_name + '-Freq[Hz]')
 freq1_sub = interpolate(freq1_out, sod1_out, sod1_sub)
 freq2_sub = interpolate(freq2_out, sod2_out, sod2_sub)
 
-## construct pyleo.series         
-series1 = pyleo.Series(time=sod1_sub, value=freq1_sub, time_name='t',
-					time_unit='s', value_name = 'Freq', 
-					value_unit='Hz', label=file1_name[0:2])
-series2 = pyleo.Series(time=sod2_sub, value=freq2_sub, time_name='t',
-					time_unit='s', value_name = 'Freq', 
-					value_unit='Hz', label=file2_name[0:2])
+time_vect, period_vect, wavelet_obj_arr, WaveletCoeff_var_arr, sub_wave_var_arr = \
+    get_plot_WaveletAnalysis_of_var_vect(sod1_sub, freq1_sub, period_range=np.array([2,500]), num_periods=100)
+WaveletCoeff_var_arr = np.transpose(WaveletCoeff_var_arr)
 
-## power spectral transform
-# Fourier transform
-psd1 = series1.spectral(method='wwz')
-psd2 = series2.spectral(method='wwz')
-# wavelet transform
-cwt1 = series1.wavelet(method='wwz')
-cwt2 = series2.wavelet(method='wwz')
+## plot wavelet coefficient spectrum
+plt.figure()
+timee, periodd = np.meshgrid(time_vect, period_vect)
+plt.pcolor(timee, periodd, WaveletCoeff_var_arr.real)
+plt.yscale('log')
 
-## wavelet coherence
-coh = series2.wavelet_coherence(series1, method='wwz')
-coh.wtc[coh.wtc>1] = np.nan
+## extract wavelet component
+period_lb, period_ub = 14, 25
+period_slc_ind = np.where((period_vect > period_lb) & (period_vect < period_ub))[0]
+period_slc = period_vect[period_slc_ind]
+sub_wave_var_slc = sub_wave_var_arr[:,period_slc_ind]
 
-# ## figure 1: plots of series1 and series2 together
-# fig1 = make_subplots(rows=2, cols=1, shared_xaxes=True, vertical_spacing=0.02)
+plt.figure()
+plt.subplot(3,1,1)
+plt.plot(sod1_sub, freq1_sub)
+plt.subplot(3,1,2)
+plt.plot(sod1_sub, np.sum(sub_wave_var_arr, axis=1))
+plt.subplot(3,1,3)
+plt.plot(sod1_sub, np.sum(sub_wave_var_slc, axis=1))
 
-# fig1.add_trace(go.Scatter(x=sod1_sub, y=freq1_sub, mode='lines', name='Bd'), row=1, col=1)
-# fig1.update_yaxes(title=file1_name[0:2]+' Freq [Hz]', row=1, col=1)
-# fig1.add_trace(go.Scatter(x=sod2_sub, y=freq2_sub, mode='lines', name='Hh'), row=2, col=1)
-# fig1.update_yaxes(title=file2_name[0:2]+' Freq [Hz]', row=2, col=1)
+## calculate wavelet power spectrum
+WaveletPower_var_arr = np.abs(WaveletCoeff_var_arr)**2 * period_vect.reshape(-1,1)
 
-# fig1.show()
+plt.figure()
+plt.pcolor(timee, periodd, WaveletPower_var_arr)
+plt.colorbar()
+plt.yscale('log')
 
-# if save_or_not == 1:
-#     fig1.write_html(save_dir + file1_name[0:2] + '-' + file2_name[0:2] + '-' + str_beg + '-' + str_end + '-TimeSeries.html')
+## select time interval for calculating power index
+sod_lb, sod_ub = 27900, 28000
+sod_slc_ind = np.where((time_vect > sod_lb) & (time_vect < sod_ub))[0]
+sod_slc = time_vect[sod_slc_ind]
+WaveletPower_var_slc = WaveletPower_var_arr[:, sod_slc_ind]
 
-## figure 2: coherence and time lag spectrum
-fig2 = make_subplots(rows=3, cols=1, shared_xaxes=True, shared_yaxes=True, vertical_spacing=0.02)
+## calculate power index
+freq_vect = 1/period_vect
+ps_mean = np.mean(WaveletPower_var_slc, 1)
 
-# time series
-fig2.add_trace(go.Scatter(x=sod1_sub, y=freq1_sub, mode='lines', name=file1_name[0:2]), row=1, col=1)
-fig2.update_yaxes(title='Freq [Hz]', row=1, col=1)
-fig2.add_trace(go.Scatter(x=sod2_sub, y=freq2_sub, mode='lines', name=file2_name[0:2]), row=1, col=1)
-fig2.update_yaxes(title='Freq [Hz]', row=1, col=1)
+plt.figure()
+plt.plot(freq_vect, ps_mean)
+plt.xscale('log')
+plt.yscale('log')
 
-# # coherence spectrum
-# fig2.add_trace(go.Contour(x=coh.time, y=coh.scale, z=np.flipud(np.rot90(coh.wtc)),
-#                          colorscale='magma', colorbar_title_text='Coherence', colorbar_y=0.7, colorbar_len=0.2), 
-#               row=1, col=1)
-# fig2.add_trace(go.Scatter(x=coh.time, y=coh.coi, mode='lines', line=dict(dash='dash')), row=1, col=1)
-# fig2.update_yaxes(range=[np.log10(np.min(coh.scale)), np.log10(np.max(coh.scale))], type='log', title='Scale [s]', row=1, col=1)
+plt.show()
 
-# time lag data and coherence background
-fig2.add_trace(go.Contour(x=coh.time, y=coh.scale, z=np.flipud(np.rot90(coh.wtc)),
-                         colorscale='magma', colorbar_title_text='Coherence', colorbar_y=0.5, colorbar_len=0.2), 
-              row=2, col=1)
-fig2.add_trace(go.Heatmap(x=coh.time, y=coh.scale, z=np.flipud(np.rot90(coh.phase/2/np.pi/coh.frequency[np.newaxis,:])), 
-                         colorscale='RdBu', colorbar_title_text='Lag [s]', colorbar_y=0.1, colorbar_len=0.2), 
-              row=2, col=1)
-fig2.add_trace(go.Scatter(x=coh.time, y=coh.coi, mode='lines', line=dict(dash='dash')), row=2, col=1)
-fig2.update_yaxes(range=[np.log10(np.min(coh.scale)), np.log10(np.max(coh.scale))], type='log', title='Scale [s]', row=2, col=1)
-
-# time lag spectrum
-fig2.add_trace(go.Contour(x=coh.time, y=coh.scale, z=np.flipud(np.rot90(coh.phase/2/np.pi/coh.frequency[np.newaxis,:])), 
-                         colorscale='RdBu', colorbar_title_text='Lag [s]', colorbar_y=0.1, colorbar_len=0.2), 
-              row=3, col=1)
-fig2.add_trace(go.Scatter(x=coh.time, y=coh.coi, mode='lines', line=dict(dash='dash')), row=3, col=1)
-fig2.update_yaxes(range=[np.log10(np.min(coh.scale)), np.log10(np.max(coh.scale))], type='log', title='Scale [s]', row=3, col=1)
-
-fig2.update_xaxes(title_text='Time [s]', row=3, col=1)
-fig2.update_layout(title={'text': file2_name[0:2] + ' relative to ' + file1_name[0:2] + '-Freq [Hz]','x': 0.5, 'y': 0.95})
-
-if save_or_not == 1:
-    # fig2.write_html(save_dir + file1_name[0:2] + '-' + file2_name[0:2] + '-' + str_beg + '-' + str_end + '-PhaseLag.html')
-    fig2.write_html(save_dir + file1_name[0:2] + '-' + file2_name[0:2] + '-' + str_beg + '-' + str_end + '-Summary.html')
-
-# fig2.show()
-
-# db
+db
