c38559b7c0b1a7a60dbca0d93f8cf6513b9dcda4
Wenger-In <97772906+Wenger-In@users.noreply.github.com>
2024-08-10 13:23:37 +0800

    Update analyse_wavelet_coherence.py

--- analyse_wavelet_coherence.py
+++ analyse_wavelet_coherence.py
@@ -15,7 +15,7 @@ from frequency_analyse_utils import convert_to_second_of_day, convert_to_HHMM, \
 
 ## import data
 i_case = 3
-save_or_not = 1
+save_or_not = 0
 if i_case == 1: # 2021/09/30(273), 12:00-13:00, Ht-Sv, Ht-Wz, Sv-Wz, (latitudinal fluctuaion)
     file_dir = 'E:/Research/Data/Tianwen/m1930x/'
     save_dir = 'E:/Research/Work/tianwen_IPS/m1930x/'
@@ -40,13 +40,13 @@ elif i_case == 3: # 2021/10/04(277), 05:40-08:20, Js-Bd, Bd-Yg, Yg-Hh, (inward p
     file_dir = 'E:/Research/Data/Tianwen/m1a04x_renew/'
     save_dir = 'E:/Research/Work/tianwen_IPS/m1a04x_renew/'
     file_Js = 'JsJschan3_1frephase1s.dat' # time has been formatted as 'seconds of day'
-    file_Bd = 'BdBdchan3_1frephase1s.dat'
+    file_Bd = 'BdBdchan3_1frephase1s.dat' # 1s, 4s
     file_Yg = 'YgYgchan3_1frephase4s.dat'
-    file_Hh = 'HhHhchan3_1frephase4s.dat' # 'HhHhchan3_1frephase1s.dat'
+    file_Hh = 'HhHhchan3_1frephase4s.dat' # 1s
     file1_name = file_Yg
     file2_name = file_Hh
-    time_beg = 2021277064100
-    time_end = 2021277065500
+    time_beg = 2021277061500
+    time_end = 2021277064500
 elif i_case == 4: # 2021/10/07(280), 03:30-04:00, sh-km, (polar region fluctuation)
     file_dir = 'E:/Research/Data/Tianwen/m1a07x/'
     save_dir = 'E:/Research/Work/tianwen_IPS/m1a07x/'
@@ -78,6 +78,39 @@ elif i_case == 6: # 2021/10/15(288), 07:40-13:00, Bd-Ys, Bd-Hh, Ys-Hh, Js-Bd, Js
     file2_name = file_Hh
     time_beg = 2021288110000
     time_end = 2021288112200
+elif i_case == 7: # 2023/10/28(301), Ht, S6, Sv
+    file_dir = 'E:/Research/Data/Tianwen/m3a28x/'
+    save_dir = 'E:/Research/Work/tianwen_IPS/m3a28x/'
+    file_Ht = 'HTHTsignum113frephase1s.dat' # 04:00-09:00
+    file_S6 = 'S6S6signum113frephase1s.dat' # 04:00-09:00
+    file_Sv = 'SVSVsignum113frephase1s.dat' # 07:50-09:00
+    file1_name = file_Ht
+    file2_name = file_S6
+    time_beg = 2023301083000
+    time_end = 2023301085900
+elif i_case == 8: # 2023/11/14(318), CD, Hh, Sv, Ur
+    file_dir = 'E:/Research/Data/Tianwen/m3b14x/'
+    save_dir = 'E:/Research/Work/tianwen_IPS/m3b14x/'
+    file_CD = 'CDCDsignum113frephase2s.dat' # 02:00-06:14, 1s
+    file_Hh = 'HHHHsignum113frephase2s.dat' # 04:00-08:30
+    file_Sv = 'SVSVsignum113frephase2s.dat' # 08:50-10:10
+    file_Ur = 'URURsignum113frephase2s.dat' # 03:00-10:00, 1s
+    file1_name = file_Sv
+    file2_name = file_Ur
+    time_beg = 2023318093000
+    time_end = 2023318100000
+elif i_case == 9: # 2023/11/16(320), Hh, Sv, TM, Ur
+    file_dir = 'E:/Research/Data/Tianwen/m3b16x/'
+    save_dir = 'E:/Research/Work/tianwen_IPS/m3b16x/'
+    file_Hh = 'HHHHsignum113frephase2s.dat' # 04:00-07:00
+    file_Sv = 'SVSVsignum113frephase2s.dat' # 09:00-10:00
+    file_TM = 'TMTMsignum113frephase1s.dat' # 02:20-08:00, 1s
+    file_Ur = 'URURsignum113frephase1s.dat' # 02:20-10:00, 1s
+    file1_name = file_TM
+    file2_name = file_Ur
+    time_beg = 2023320023000
+    time_end = 2023320030000
+    
 # convert subplot time interval
 str_beg = str(time_beg)[-6:-2]
 str_end = str(time_end)[-6:-2]
@@ -101,6 +134,8 @@ if i_case == 6 and file2_name == 'HhHhchan3_1frephase1s.dat':
     sod2 = time2 # time has been formatted as 'seconds of day'
 freq1 = data1[:,1]
 freq2 = data2[:,1]
+if i_case == 7 and file2_name == 'S6S6signum113frephase1s.dat':
+    freq2 = -freq2 # frequency should take its negative value
 
 ## select time interval
 ind1_sub = np.where((sod1 > sod_beg) & (sod1 < sod_end))
@@ -116,8 +151,8 @@ freq2_sub = freq2[ind2_sub]
 freq1_out, sod1_out = eliminate_outliers(freq1_sub, sod1_sub, 10) #3
 freq2_out, sod2_out = eliminate_outliers(freq2_sub, sod2_sub, 10) #3
 # step 2: detrend for frequency sequence
-freq1_fit, freq1_detrend = detrend(freq1_out, sod1_out, 5)
-freq1_fit, freq2_detrend = detrend(freq2_out, sod2_out, 5)
+freq1_fit, freq1_detrend = detrend(freq1_out, sod1_out, 3) #3
+freq1_fit, freq2_detrend = detrend(freq2_out, sod2_out, 3)
 # step 3: interpolation for frequency sequence
 freq1_interp = interpolate(freq1_detrend, sod1_out, sod1_sub)
 freq2_interp = interpolate(freq2_detrend, sod2_out, sod2_sub)
@@ -133,6 +168,12 @@ series1 = pyleo.Series(time=sod1_sub, value=freq1_interp, \
     time_name='Time', time_unit='s', value_name = 'Freq', value_unit='Hz', label=file1_name[0:2])
 series2 = pyleo.Series(time=sod2_sub, value=freq2_interp, \
     time_name='Time', time_unit='s', value_name = 'Freq', value_unit='Hz', label=file2_name[0:2])
+
+## lowpass filter cutoff at 0.1 Hz
+series1 = series1.interp().filter(cutoff_freq=0.1)
+series2 = series2.interp().filter(cutoff_freq=0.1)
+freq1_interp = series1.value
+freq2_interp = series2.value
 
 # ## transform analysis
 # # Fourier transform
@@ -188,7 +229,7 @@ fig.update_layout(title={'text': file2_name[0:2] + ' relative to ' + file1_name[
     'x': 0.5, 'y': 0.95})
 
 if save_or_not == 1:
-    fig.write_html(save_dir + file1_name[0:2] + '-' + file2_name[0:2] + '-' + str_beg + '-' + str_end + '-4s-Summary.html')
+    fig.write_html(save_dir + file1_name[0:2] + '-' + file2_name[0:2] + '-' + str_beg + '-' + str_end + '-Summary.html')
     # fig.write_image(save_dir + file1_name[0:2] + '-' + file2_name[0:2] + '-' + str_beg + '-' + str_end + '-Summary.png')
 else:
     fig.show()
